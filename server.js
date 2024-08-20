1089a91964db971aafdd418f0109a17159c945e2
Sangma83 <poushalisangma13@gmail.com>
2024-08-20 20:44:23 +0600

    firebase link added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://mern-ecommerce-project-4e3ff.web.app'],
   credentials: true,
 }));
 app.use(express.json());
